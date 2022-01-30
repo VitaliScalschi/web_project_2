aedf23bd710e6d5377a5c6a5c83361c94b0203a6
Vitali PC_Home <scalschi.vitali@gmail.com>
2022-01-30 22:27:03 +0200

    update

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        // adaptiveHeight: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/right.svg" alt=""></button>',
+    });
+  });
