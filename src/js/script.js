2f3d5c254f75df690e2f04e5eed8c4cbf47604f2
Vitali <scalschi.vitali@gmial.com>
2022-01-31 16:38:24 +0200

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,27 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icon/right.svg" alt=""></button>',
     });
-  });
+
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+      });
+
+
+     
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_activ');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_activ');
+            });
+        });
+    }
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+});
