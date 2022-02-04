848ae666de8311c51db086565ccc06463fe1327f
Vitali <scalschi.vitali@gmial.com>
2022-02-04 16:48:35 +0200

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -6,16 +6,14 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icon/right.svg" alt=""></button>',
     });
 
-
+    ///Taburile la compartimentul catalog.
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
-      });
+      });    
 
-
-     
-
+    //////Functionarea butonului de  informare la comparimentul catalog
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -28,4 +26,60 @@ $(document).ready(function(){
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
 
+
+    ///Modal 
+
+    $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button__catalog-item').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3
+                },
+                phone:"required",
+                email:{
+                    required: true,
+                    email:true
+                }
+            },
+            messages: {
+                name: {
+                    required :"Introduceti datele",
+                    minlength: jQuery.validator.format("Introduceti {0} simboluri")
+                },
+                email: {
+                  required: "Introduceti email",
+                  email: "Your email address must be in the format of name@domain.com"
+                },
+                phone: {
+                    required: "Introduceti nr de telefon"
+                }
+    
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+
+
+    $('input[name=phone]').mask("(+373) 999-9999");
 });
