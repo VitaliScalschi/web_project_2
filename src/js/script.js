71df87e12bee52b52b6a0c0a53d4c1a2c3bcc425
Vitali <scalschi.vitali@gmial.com>
2022-02-11 15:36:54 +0200

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -117,9 +117,13 @@ $(document).ready(function(){
     
 
 
-    $("a[href^='#']").click(function(){
-        const _href = $(this).attr ("href");
+    $("a[href = #up]").click(function(){
+        const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
+
+    new WOW().init();
+
+
 });
